68e0ab0bfc5ea9e7577610767e8696838c0f9ecf
Marlon Monsores <marlonmonsores@live.com>
2023-08-29 21:33:49 -0300

    :construction: Create elements in page information irrigation

--- src/app/pages/infoReg/page.tsx
+++ src/app/pages/infoReg/page.tsx
@@ -4,7 +4,39 @@ import styles from './infoRegPage.module.sass'
 export default function InfoRegPage() {
   return (
     <section className={styles.infoRegContainer}>
-      <h1>pagina info rega</h1>
+      <div className={styles.wrapperTitle}>
+        <div className={styles.boxField}>
+          <span>Entrada última rega:</span>
+          <p>100 PPM</p>
+        </div>
+        <div className={styles.boxField}>
+          <span>Saída última rega:</span>
+          <p>100 PPM</p>
+        </div>
+      </div>
+      <h1>Nova Rega</h1>
+      <section className={styles.entryValue}>
+        <div className={styles.boxInput}>
+            <label>Entrada PPM:</label>
+            <input type="text" />
+        </div>
+        <div className={styles.boxInput}>
+            <label>Entrada PH:</label>
+            <input type="text" />
+        </div>
+        <button>Enviar</button>
+      </section>
+      <section className={styles.exitValue}>
+        <div className={styles.boxInput}>
+            <label>Saída PPM:</label>
+            <input type="text" />
+        </div>
+        <div className={styles.boxInput}>
+            <label>Saída PH:</label>
+            <input type="text" />
+        </div>
+        <button>Enviar</button>
+      </section>
     </section>
   )
 }
