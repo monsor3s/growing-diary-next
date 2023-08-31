84a5d702e8d15957bcdcaae0d88a4998495d8462
monsor3s <64220885+monsor3s@users.noreply.github.com>
2023-08-30 21:00:14 -0300

    :lipstick: Finished styles page irrigation

--- src/app/pages/infoReg/page.tsx
+++ src/app/pages/infoReg/page.tsx
@@ -1,42 +1,50 @@
 import React from 'react'
 import styles from './infoRegPage.module.sass'
+import Button from '@/objects/Button/Button'
 
 export default function InfoRegPage() {
   return (
     <section className={styles.infoRegContainer}>
       <div className={styles.wrapperTitle}>
+        <h1>Valores última rega</h1>
         <div className={styles.boxField}>
-          <span>Entrada última rega:</span>
+          <span>Entrada PPM:</span>
           <p>100 PPM</p>
+          <span>Entrada PH:</span>
+          <p>6.0 PH</p>
         </div>
         <div className={styles.boxField}>
-          <span>Saída última rega:</span>
+          <span>Saída PPM:</span>
           <p>100 PPM</p>
+          <span>Saída PH:</span>
+          <p>6.0 PH</p>
         </div>
       </div>
-      <h1>Nova Rega</h1>
-      <section className={styles.entryValue}>
-        <div className={styles.boxInput}>
-            <label>Entrada PPM:</label>
-            <input type="text" />
-        </div>
-        <div className={styles.boxInput}>
-            <label>Entrada PH:</label>
-            <input type="text" />
-        </div>
-        <button>Enviar</button>
-      </section>
-      <section className={styles.exitValue}>
-        <div className={styles.boxInput}>
-            <label>Saída PPM:</label>
-            <input type="text" />
-        </div>
-        <div className={styles.boxInput}>
-            <label>Saída PH:</label>
-            <input type="text" />
-        </div>
-        <button>Enviar</button>
-      </section>
+      <div className={styles.wrapperBody}>
+        <h1>Nova Rega</h1>
+        <section className={styles.entryValue}>
+          <div className={styles.boxInput}>
+              <label>Entrada PPM:</label>
+              <input type="text" />
+          </div>
+          <div className={styles.boxInput}>
+              <label>Entrada PH:</label>
+              <input type="text" />
+          </div>
+          <Button />
+        </section>
+        <section className={styles.exitValue}>
+          <div className={styles.boxInput}>
+              <label>Saída PPM:</label>
+              <input type="text" />
+          </div>
+          <div className={styles.boxInput}>
+              <label>Saída PH:</label>
+              <input type="text" />
+          </div>
+          <Button />
+        </section>
+      </div>
     </section>
   )
 }
